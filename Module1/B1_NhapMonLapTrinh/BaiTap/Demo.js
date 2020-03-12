644c0a77ad1f9c6a0dbb622a0ce5800577bc97e0
VuManhHung <ngovuhunghy@gmail.com>
2020-03-12 10:42:43 +0700

    task1

---
+++ Module1/B1_NhapMonLapTrinh/BaiTap/Demo.js
@@ -0,0 +1,8 @@
+array[x] = document.getElementById("txtValue").value;
+alert("Element: " + array[x] + " Added at index " + x);
+x++;
+document.getElementById("txtValue ").value = "";
+
+function display_array() {
+    let e = "<hr/>";
+}
---
+++ Module1/B1_NhapMonLapTrinh/BaiTap/aaaa.js
@@ -0,0 +1,6 @@
+function footToMeter(foot) {
+    return 0.305 * foot
+}
+function meterToFoot(metter) {
+    return 3.279 * meter
+}
